28b2a074d38bd48dc8d0f4f2325bd7ae55934457
Sam <samroux808@gmail.com>
2025-06-24 19:41:36 -0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,23 @@
+import { Router, Application } from "oak";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (context) => {
+  context.response.body = Deno.readTextFileSync("./static/index.html");
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (context, next) => {
+  const root = "./static";
+  try {
+    await context.send({ root });
+  } catch {
+    next();
+  }
+});
+
+app.listen({ port: 8000 });
+console.log("Server is running on http://localhost:8000");
